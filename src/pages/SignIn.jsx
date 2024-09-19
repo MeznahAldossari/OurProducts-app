14814c664f95932059937af129adeaad117a63cd
MeznahAldossari <MeznahAldossari@hotmail.com>
2024-09-19 11:40:36 +0300

    Logo Updated by Meznah Fayez

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
-import logo from '../assets/onlineStore.png'
 import { Link, useNavigate } from "react-router-dom";
 import axios from 'axios';
+import logo from '../assets/myLogo.png'
+
 
 
 
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import logo from '../assets/onlineStore.png'
+import logo from '../assets/myLogo.png'
 import { Link, useNavigate } from "react-router-dom";
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
